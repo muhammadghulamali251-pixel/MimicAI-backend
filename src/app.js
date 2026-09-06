3e9b67c0634acb828eba84d534ea113844b29444
Muhammad Ghulam Ali <mghulamali888@gmail.com>
2026-09-06 19:11:54 +0500

    Update APP_URL

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express ()
 // Middlewares
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: 'https://mimic-ai.netlify.app',
     credentials: true
 }))
 
